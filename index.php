63616526c7b9b7b5f5cb94a18c52679141af47bc
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2023-09-20 11:30:02 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ventana de Noticias</title>
+    <link rel="stylesheet" href="styles.css">
+
+
+<body>
+    <div class="noticia-container">
+        <div class="imagen-noticia">
+            <img src="https://w0.peakpx.com/wallpaper/654/174/HD-wallpaper-vertical-building.jpg" alt="Imagen de la Noticia">
+        </div>
+        <div class="noticia-contenido">
+            <h1 class="noticia-titulo">APARTAMENTOS DISPONIBLES</h1>
+            <p class="noticia-fecha">Fecha de Publicación: 18 de Septiembre de 2023</p>
+            <div class="noticia-cuerpo">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum nec justo id bibendum. Vivamus pharetra, metus a rutrum viverra, dolor arcu scelerisque justo, at ultricies neque justo at purus.
+            
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla condimentum nec justo id bibendum. Vivamus pharetra, metus a rutrum viverra, dolor arcu scelerisque justo, at ultricies neque               
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quo laudantium voluptates natus perferendis, est quaerat labore quis obcaecati maiores, repellendus aut neque! Iste temporibus aperiam quod doloribus error libero.
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia molestias deserunt cumque, esse a voluptates porro eaque, laborum debitis maiores architecto sit q
+            </div>
+        </div>
+    </div>
+</body>
+</head>
+</html>
